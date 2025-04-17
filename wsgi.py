2c7a809379dfc148536241accb463b2c4b840cec
KCC-IT-EDU <itedu@khodiyarcadcenter.com>
2025-04-17 16:45:58 +0530

    c3

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,4 @@
 from app import app
-from flask import request, redirect
-
-# Force HTTPS
-@app.before_request
-def before_request():
-    if not request.is_secure and app.env != 'development':
-        url = request.url.replace('http://', 'https://', 1)
-        return redirect(url, code=301)
 
 if __name__ == "__main__":
     app.run() 
